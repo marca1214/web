9a0cd67ded395fd1608f3c6dd66c1a6a9ddd6c1c
marca1214 <andressantacruz1214@gmail.com>
2024-09-28 19:45:55 -0400

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -17,8 +17,8 @@ $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'escribeaquitucorreo@hotmail.com';
-$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+$para = 'andressantacruz1214@gmail.com';
+$asunto = 'Asunto 1234';
 
 mail($para, $asunto, utf8_decode($message), $header);
 
